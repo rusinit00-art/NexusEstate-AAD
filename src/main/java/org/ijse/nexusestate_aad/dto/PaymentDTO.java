4093512d6451cc592758ae96c6d014aadaeb3970
rusinit00-art <rusinit00@gmail.com>
2026-08-15 19:20:16 +0530

    Add payment Section

--- src/main/java/org/ijse/nexusestate_aad/dto/PaymentDTO.java
+++ src/main/java/org/ijse/nexusestate_aad/dto/PaymentDTO.java
@@ -1,4 +1,18 @@
 package org.ijse.nexusestate_aad.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.ijse.nexusestate_aad.enumiration.PaymentMethod;
+import org.ijse.nexusestate_aad.enumiration.PaymentStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDTO {
+    private Long id;
+    private Double amount;
+    private PaymentMethod method;
+    private PaymentStatus status;
+    private Long transactionId;
 }
