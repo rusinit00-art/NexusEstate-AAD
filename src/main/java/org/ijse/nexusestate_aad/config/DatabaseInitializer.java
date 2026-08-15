d014d8e206dbb8bed9fb0f0be17e57ad47e6b4da
rusinit00-art <rusinit00@gmail.com>
2026-08-15 17:55:17 +0530

    update class

--- src/main/java/org/ijse/nexusestate_aad/config/DatabaseInitializer.java
+++ src/main/java/org/ijse/nexusestate_aad/config/DatabaseInitializer.java
@@ -1,7 +1,11 @@
 package org.ijse.nexusestate_aad.config;
 
 import lombok.RequiredArgsConstructor;
+import org.ijse.nexusestate_aad.entity.PropertyFoundation.Location;
+import org.ijse.nexusestate_aad.entity.PropertyFoundation.PropertyCategory;
 import org.ijse.nexusestate_aad.entity.SecurityandUsers.Role;
+import org.ijse.nexusestate_aad.repository.CategoryRepository;
+import org.ijse.nexusestate_aad.repository.LocationRepository;
 import org.ijse.nexusestate_aad.repository.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +16,8 @@ import java.util.List;
 public class DatabaseInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepo;
+    private final CategoryRepository catRepo;
+    private final LocationRepository locRepo;
 
     @Override
     public void run(String... args) {
@@ -22,6 +28,23 @@ public class DatabaseInitializer implements CommandLineRunner {
                     new Role(null, "BUYER")
             ));
             System.out.println("✅ Database Initialized with System Roles!");
+        }
+        if (catRepo.count() == 0) {
+            catRepo.saveAll(List.of(
+                    new PropertyCategory(null, "House"),
+                    new PropertyCategory(null, "Land"),
+                    new PropertyCategory(null, "Apartment")
+            ));
+            System.out.println("✅ Property Categories Initialized!");
+        }
+
+        if (locRepo.count() == 0) {
+            locRepo.saveAll(List.of(
+                    new Location(null, "Colombo", "Western"),
+                    new Location(null, "Kandy", "Central"),
+                    new Location(null, "Galle", "Southern")
+            ));
+            System.out.println("✅ System Locations Initialized!");
         }
     }
 }
