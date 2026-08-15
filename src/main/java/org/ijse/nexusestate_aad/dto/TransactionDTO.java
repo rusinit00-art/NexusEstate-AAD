f7f2c813cd3f124b6886b94c7a2077b32079c5cf
rusinit00-art <rusinit00@gmail.com>
2026-08-15 19:11:19 +0530

    Add transaction section

--- src/main/java/org/ijse/nexusestate_aad/dto/TransactionDTO.java
+++ src/main/java/org/ijse/nexusestate_aad/dto/TransactionDTO.java
@@ -1,4 +1,17 @@
 package org.ijse.nexusestate_aad.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionDTO {
+    private Long id;
+    private Double finalPrice;
+    private LocalDateTime saleDate;
+    private Long propertyId;
+    private Long buyerId;
 }
