4ff061c273594d1f93f7992587bfe2987be1a317
rusinit00-art <rusinit00@gmail.com>
2026-08-19 15:06:11 +0530

    Add

--- src/main/java/org/ijse/nexusestate_aad/repository/CategoryRepository.java
+++ src/main/java/org/ijse/nexusestate_aad/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import org.ijse.nexusestate_aad.entity.PropertyFoundation.PropertyCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<PropertyCategory, Long> {
+    Optional<PropertyCategory> findByName(String name);
 }
--- src/main/java/org/ijse/nexusestate_aad/repository/LocationRepository.java
+++ src/main/java/org/ijse/nexusestate_aad/repository/LocationRepository.java
@@ -4,6 +4,9 @@ import org.ijse.nexusestate_aad.entity.PropertyFoundation.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByCity(String city);
 }
